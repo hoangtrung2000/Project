7db82b1c63bb97161886368b21c7ed37efab767f
tranminhduc020100 <minhhducc201@gmail.com>
2019-12-05 17:40:18 +0700

    index

--- WindowsFormsApplication1/WindowsFormsApplication1/IndexForm.Designer.cs
+++ WindowsFormsApplication1/WindowsFormsApplication1/IndexForm.Designer.cs
@@ -28,11 +28,81 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.grdViewAllEmployees = new System.Windows.Forms.DataGridView();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.btnCreate = new System.Windows.Forms.ToolStripMenuItem();
+            this.btnDelete = new System.Windows.Forms.ToolStripMenuItem();
+            this.btnSalary = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.grdViewAllEmployees)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // grdViewAllEmployees
+            // 
+            this.grdViewAllEmployees.AllowUserToAddRows = false;
+            this.grdViewAllEmployees.AllowUserToDeleteRows = false;
+            this.grdViewAllEmployees.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.grdViewAllEmployees.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.grdViewAllEmployees.Location = new System.Drawing.Point(0, 24);
+            this.grdViewAllEmployees.Name = "grdViewAllEmployees";
+            this.grdViewAllEmployees.ReadOnly = true;
+            this.grdViewAllEmployees.Size = new System.Drawing.Size(284, 237);
+            this.grdViewAllEmployees.TabIndex = 0;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.btnCreate,
+            this.btnDelete,
+            this.btnSalary});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(284, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // btnCreate
+            // 
+            this.btnCreate.Name = "btnCreate";
+            this.btnCreate.Size = new System.Drawing.Size(53, 20);
+            this.btnCreate.Text = "Create";
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(52, 20);
+            this.btnDelete.Text = "Delete";
+            // 
+            // btnSalary
+            // 
+            this.btnSalary.Name = "btnSalary";
+            this.btnSalary.Size = new System.Drawing.Size(50, 20);
+            this.btnSalary.Text = "Salary";
+            // 
+            // IndexForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.grdViewAllEmployees);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "IndexForm";
             this.Text = "IndexForm";
+            ((System.ComponentModel.ISupportInitialize)(this.grdViewAllEmployees)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView grdViewAllEmployees;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem btnCreate;
+        private System.Windows.Forms.ToolStripMenuItem btnDelete;
+        private System.Windows.Forms.ToolStripMenuItem btnSalary;
     }
 }
--- WindowsFormsApplication1/WindowsFormsApplication1/IndexForm.cs
+++ WindowsFormsApplication1/WindowsFormsApplication1/IndexForm.cs
@@ -12,9 +12,51 @@ namespace WindowsFormsApplication1
 {
     public partial class IndexForm : Form
     {
+        private EmployeeManagement Business;
         public IndexForm()
         {
             InitializeComponent();
+            this.Business = new EmployeeManagement();
+            this.Load += IndexForm_Load;
+            this.btnCreate.Click += btnCreate_Click;
+            this.btnDelete.Click += btnDelete_Click;
+            this.btnSalary.Click += btnSalary_Click;
+            this.grdViewAllEmployees.DoubleClick += grdViewAllEmployees_DoubleClick;
+        }
+
+        void grdViewAllEmployees_DoubleClick(object sender, EventArgs e)
+        {
+       
+        }
+
+        void btnSalary_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        void btnDelete_Click(object sender, EventArgs e)
+        {
+           if(this.grdViewAllEmployees.SelectedRows.Count ==1)
+           {
+               if (MessageBox.Show("Do you want delete this?", "Confirm", MessageBoxButtons.YesNo) == System.Windows.Forms.DialogResult.Yes);
+           }
+        }
+
+        void btnCreate_Click(object sender, EventArgs e)
+        {
+            var form = new CreateForm();
+            form.ShowDialog();
+            this.LoadAllEmployees();
+        }
+
+        void IndexForm_Load(object sender, EventArgs e)
+        {
+            this.LoadAllEmployees();
+        }
+        void LoadAllEmployees()
+        {
+            var employees = this.Business.GetEmployees();
+            this.grdViewAllEmployees.DataSource = employees;
         }
     }
 }
