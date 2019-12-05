718cdffb8262d5c40d246895b41882cef2f1881c
tranminhduc020100 <minhhducc201@gmail.com>
2019-12-05 17:23:54 +0700

    Finish indexform

--- WindowsFormsApplication1/WindowsFormsApplication1/EmployeeManagement.cs
+++ WindowsFormsApplication1/WindowsFormsApplication1/EmployeeManagement.cs
@@ -22,6 +22,36 @@ namespace WindowsFormsApplication1
             newEmployee.Address = address;
             newEmployee.Location = location;
             newEmployee.Salary = salary;
+
+            var db = new MyDatabaseEntities();
+            db.Employees.Add(newEmployee);
+            db.SaveChanges();
         }
+        public void EditClass(int id, string code, string name, DateTime birthday, string address, string location, int salary)
+        {
+            var db = new MyDatabaseEntities();
+            var oldEmployee = db.Employees.Find(id);
+            oldEmployee.Code = code;
+            oldEmployee.Name = name;
+            oldEmployee.Birthday = birthday;
+            oldEmployee.Address = address;
+            oldEmployee.Location = location;
+            oldEmployee.Salary = salary;
+            db.Entry(oldEmployee).State = System.Data.Entity.EntityState.Modified;
+            db.SaveChanges();
+        }
+        public void DeleteEmployee(int id)
+        {
+            var db = new MyDatabaseEntities();
+            var employee = db.Employees.Find(id);
+            db.Employees.Remove(employee);
+            db.SaveChanges();
+        }
+        public Employee GetEmployee(int id)
+        {
+            var db = new MyDatabaseEntities();
+            return db.Employees.Find(id);
+        }
+
     }
 }
